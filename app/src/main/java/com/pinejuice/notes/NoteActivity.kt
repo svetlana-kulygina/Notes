2701a546e6657deec56d38755c465cf9cd00e555
Svetlana Kulygina <svetlana.kulygina@outlook.com>
2018-03-12 14:28:14 +0300

    Note action menu

--- app/src/main/java/com/pinejuice/notes/NoteActivity.kt
+++ app/src/main/java/com/pinejuice/notes/NoteActivity.kt
@@ -1,16 +1,54 @@
 package com.pinejuice.notes
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 
 class NoteActivity : SlideActivity() {
 
+    private var noteExists = false
+    private var menu: Menu? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
+        if (savedInstanceState?.get("key") != null) {
+            noteExists = true
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.action_bar_note, menu)
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        this.menu = menu
+        val save = menu.findItem(R.id.action_save)
+        val edit = menu.findItem(R.id.action_edit)
+        if (noteExists) {
+            edit?.isVisible = true
+        } else {
+            save?.isVisible = true
+        }
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+        R.id.action_save -> {
+            item.isVisible = false
+            menu?.findItem(R.id.action_edit)?.isVisible = true
+            true
+        }
+
+        R.id.action_edit -> {
+            item.isVisible = false
+            menu?.findItem(R.id.action_save)?.isVisible = true
+            true
+        }
+
         android.R.id.home -> {
             finish()
             true
