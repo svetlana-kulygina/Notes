c27cff2159351f93452882d1bfca1fa875feca36
Svetlana Kulygina <svetlana.kulygina@outlook.com>
2018-03-20 16:36:32 +0300

    Open/save file logic

--- app/src/main/java/com/pinejuice/notes/MainActivity.kt
+++ app/src/main/java/com/pinejuice/notes/MainActivity.kt
@@ -2,14 +2,37 @@ package com.pinejuice.notes
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.widget.SimpleCursorAdapter
 import android.view.*
+import android.widget.SimpleAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : SlideActivity() {
 
+    private val ATTRIBUTE_NAME = "name"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fillList()
+    }
+
+    private fun fillList() {
+        val allNotes = getExternalFilesDir(null).list()
+        if (allNotes == null || allNotes.isEmpty()) {
+            emptyListHint.visibility = View.VISIBLE
+            return
+        }
+        val data = ArrayList<Map<String, Any>>()
+        for (title in allNotes) {
+            val m = HashMap<String, Any>()
+            m.put(ATTRIBUTE_NAME, title)
+            data.add(m)
+        }
+        val from = arrayOf(ATTRIBUTE_NAME)
+        val to = intArrayOf(R.id.text1)
+        listView.adapter = SimpleAdapter(this, data, R.layout.list_item, from, to)
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
--- app/src/main/java/com/pinejuice/notes/NoteActivity.kt
+++ app/src/main/java/com/pinejuice/notes/NoteActivity.kt
@@ -1,16 +1,29 @@
 package com.pinejuice.notes
 
+import android.net.Uri
 import android.os.Bundle
 import android.text.InputType
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.action_bar_note_custom.*
 import kotlinx.android.synthetic.main.activity_note.*
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
+import android.provider.MediaStore
+import java.lang.Exception
+import java.nio.file.Files
+
 
 class NoteActivity : SlideActivity() {
 
+    private val filenamePattern = "[?|\\/\":*<>\n]+"
     private var noteExists = false
     private var menu: Menu? = null
+    private var file: File? = null
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +35,38 @@ class NoteActivity : SlideActivity() {
         actionBar?.setDisplayShowTitleEnabled(false)
         actionBar?.setDisplayShowCustomEnabled(true)
         actionBar?.setCustomView(R.layout.action_bar_note_custom)
+        if (intent.data != null) {
+            BufferedInputStream(contentResolver.openInputStream(intent.data)).use {
+                val bis = it
+                ByteArrayOutputStream().use {
+                    var result = bis.read()
+                    while (result != -1) {
+                        it.write(result)
+                        result = bis.read()
+                    }
+                    editNote.setText(it.toString())
+                }
+            }
+            val filePath = getRealPathFromURI(intent.data)
+            if (filePath != null) {
+                file = File(filePath)
+                val pathSplit = filePath.split("/")
+                noteTitle.setText(pathSplit.last().removeSuffix(".txt"))
+            }
+        }
+    }
+
+    private fun getRealPathFromURI(contentUri: Uri): String? {
+        try {
+            val proj = arrayOf(MediaStore.Images.Media.DATA)
+            contentResolver.query(contentUri, proj, null, null, null).use {
+                val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                it.moveToFirst()
+                return it.getString(columnIndex)
+            }
+        } catch (ex: Exception) {
+            return null
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -42,7 +87,7 @@ class NoteActivity : SlideActivity() {
         return true
     }
 
-    fun toggleCaps() {
+    private fun toggleCaps() {
         editNote.inputType = editNote.inputType.xor(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES)
     }
 
@@ -51,6 +96,7 @@ class NoteActivity : SlideActivity() {
         R.id.action_save -> {
             item.isVisible = false
             menu?.findItem(R.id.action_edit)?.isVisible = true
+            save()
             true
         }
 
@@ -82,5 +128,55 @@ class NoteActivity : SlideActivity() {
         else -> {
             super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun makeValidTitle(title: CharSequence): CharSequence {
+        return title.replace(Regex(filenamePattern), "").trim()
+    }
+
+    private fun generateTitle(directory: File): String {
+        var title: CharSequence = makeValidTitle(noteTitle.text)
+        if (title.isBlank()) {
+            title = makeValidTitle(editNote.text.take(20))
+        }
+        if (title.isBlank()) {
+            title = dateFormat.format(Date())
+        }
+        val children = directory.list()
+        if (children != null) {
+            var iterTitle = title
+            var i = 1
+            while (children.contains("$iterTitle.txt")) {
+                iterTitle = "$title ($i)"
+                i++
+            }
+            title = iterTitle
+        }
+        return "$title.txt"
+    }
+
+    private fun createFile(): File {
+        val dir = getExternalFilesDir(null)
+        val f = File(dir.absolutePath, generateTitle(dir))
+        f.createNewFile()
+        return f
+    }
+
+    private fun save() {
+        if (file == null) {
+            file = createFile()
+        } else {
+            val f = file!!
+            val currentName = f.name
+            val newName = generateTitle(f.parentFile)
+            if (currentName != newName) {
+                f.renameTo(File(f.parentFile, newName))
+            }
+        }
+        val outWriter = OutputStreamWriter(file!!.outputStream())
+        outWriter.use {
+            it.append(editNote.text)
+        }
+        Toast.makeText(this, R.string.toast_saved, Toast.LENGTH_SHORT).show()
     }
 }
